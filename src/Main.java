1e94b962eac2a2f1b200c1d394e681550df8b426
Alumno Plaza Vespucio <pv-alumno@duoc.acad>
2026-09-03 21:42:49 -0400

    JAVA-008 - Agregar control de acceso con if else

--- src/Main.java
+++ src/Main.java
@@ -17,6 +17,12 @@ public class Main {
 
         estudiante.mostrarInformacion();
 
+        if (edad >= 18){
+            System.out.println("Acceso autorizado. ");
+        } else {
+            System.out.println("Acceso restringido");
+        }
+
 
     }
 }
